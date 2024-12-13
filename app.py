b90e3ed1b697347fc4a9b7747d71825831dd9688
scchang <sc.chang@gmail.com>
2024-12-13 14:13:16 +0800

    modified

--- app.py
+++ app.py
@@ -2,38 +2,12 @@ import streamlit as st
 import pandas as pd
 
 def main():
-    # st.title("新富昌")
-    st.markdown("### 新富昌")
-    tab1, tab2, tab3 = st.tabs(["租金公共帳戶", "水錶度數記", "太陽能發電租金記錄"])
-    with tab1:
-        df = pd.read_csv("data/租金公共帳戶113年.csv")
-        st.markdown("#### 租金公共帳戶")
-        df = df.iloc[::-1]
-        df = df.fillna("")
-        st.markdown("#### 113年")
-        st.dataframe(df, use_container_width=True)
-        # st.dataframe(df)
-        # st.table(df)
-        df = pd.read_csv("data/租金公共帳戶112年.csv")
-        df = df.iloc[::-1]
-        df = df.fillna("")
-        st.markdown("#### 112年")
-        st.dataframe(df, use_container_width=True)
-    with tab2:
-        df = pd.read_csv("data/水錶度數記錄113年.csv")
-        st.markdown("#### 水錶度數記錄")
-        df = df.round(1)
-        print(df)
-        df = df.iloc[::-1]
-        df = df.fillna("")
-        st.markdown("#### 113年")
-        st.dataframe(df, use_container_width=True)
-    with tab3:
-        df = pd.read_csv("data/太陽能發電租金記錄.csv")
-        st.markdown("#### 太陽能發電租金記錄")
-        df = df.iloc[::-1]
-        df = df.fillna("")
-        st.dataframe(df, use_container_width=True)
+    st.title("新富昌")
+    # st.markdown("# 新富昌")
+    st.markdown('### 租金公共帳戶')
+    st.markdown('### 水錶度數記錄')
+    st.markdown('### 太陽能發電租金記錄')
+
     pass
 
 if __name__ == '__main__':
